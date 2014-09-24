f8d0abd551aff5ee7bdfdcc7822d57315e2c6fba
Ivan R. Judson <irjudson@gmail.com>
2014-09-24 11:42:44 -0700

    Parallels, Virtualbox working, still hacking azure.

--- nitrogen/deploy-azure.sh
+++ nitrogen/deploy-azure.sh
@@ -3,13 +3,23 @@
 # Deploy to Azure, which requires some shell magic to make things happy
 #
 
+# TODO: How do we check to see if the azure xplat tools are installed? 
 # Are the azure tools installed
 # if [ `azure -v` ]
 
+# Get the machine name from the command line or prompt for it
+if [ "x$1" = "x" ]; then
+	echo "Please enter a name for your virtual machine: "
+	read MACHINE_NAME
+	export NITROGEN_VM_NAME="$MACHINE_NAME"
+else
+	export NITROGEN_VM_NAME="$0"
+fi
+
 # Subscription
 echo -n "Retrieving subscription..."
-export AZURE_SUBSCRIPTION="`azure account show | grep ID | awk '{ print $3 }'`"
-echo "done ($AZURE_SUBSCRIPTION)."
+export AZURE_SUBSCRIPTION_ID="`azure account show | grep ID | awk '{ print $3 }'`"
+echo "done ($AZURE_SUBSCRIPTION_ID)."
 
 # Download management certificate
 echo -n "Retrieving management certificate..."
@@ -24,7 +34,17 @@ export NITROGEN_CERT="NitrogenCert.pem"
 export NITROGEN_KEY="NitrogenPrivateKey.key"
 echo "done."
 
+# Save settings
+echo export NITROGEN_VM_NAME="$NITROGEN_VM_NAME" > bash.settings
+echo export AZURE_SUBSCRIPTION_ID="$AZURE_SUBSCRIPTION_ID" >> bash.settings
+echo export AZURE_MANAGEMENT_CERT="$AZURE_MANAGEMENT_CERT" >> bash.settings
+echo export NITROGEN_KEY="$NITROGEN_KEY" >> bash.settings
+echo export NITROGEN_CERT="$NITROGEN_CERT" >> bash.settings
+
 # Build the vm
 echo "Provisioning..."
 vagrant up --provider azure
 echo -n "done."
+
+# Inform the user
+echo "To use the azure created vm, run . bash.settings before you issue any vagrant commands."
